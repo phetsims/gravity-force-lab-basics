5bf85a3cd65870707b295fb59b2011326f68920e
Jesse Greenberg <jesse.greenberg@colorado.edu>
2017-03-14 15:36:48 -0600

    refactor for phetsims/inverse-square-law-common#1

--- js/gravity-force-lab-basics/model/GravityForceLabBasicsModel.js
+++ js/gravity-force-lab-basics/model/GravityForceLabBasicsModel.js
@@ -1,7 +1,7 @@
 // Copyright 2016, University of Colorado Boulder
 
 /**
- *
+ * Main model for gravity-force-lab-basics.
  * @author Jesse Greenberg
  */
 define( function( require ) {
@@ -12,40 +12,50 @@ define( function( require ) {
   var InverseSquareLawModel = require( 'INVERSE_SQUARE_LAW_COMMON/model/InverseSquareLawModel' );
   var gravityForceLabBasics = require( 'GRAVITY_FORCE_LAB_BASICS/gravityForceLabBasics' );
   var GravityForceLabBasicsConstants = require( 'GRAVITY_FORCE_LAB_BASICS/gravity-force-lab-basics/GravityForceLabBasicsConstants' );
-  var InverseSquareLawModes = require( 'INVERSE_SQUARE_LAW_COMMON/InverseSquareLawModes' );
+  var Property = require( 'AXON/Property' );
+  var InverseSquareLawCommonConstants = require( 'INVERSE_SQUARE_LAW_COMMON/InverseSquareLawCommonConstants' );
+  var Mass = require( 'INVERSE_SQUARE_LAW_COMMON/model/Mass' );
+  var Color = require( 'SCENERY/util/Color' );
+
+  // phet-io modules
+  var TBoolean = require( 'ifphetio!PHET_IO/types/TBoolean' );
 
   /**
    * @constructor
+   * @param {Tandem} tandem
    */
   function GravityForceLabBasicsModel( tandem ) {
 
     // initial masses
     var massMultiplier = GravityForceLabBasicsConstants.BILLION_MULTIPLIER;
-    var mass1 = 2 * massMultiplier;
-    var mass2 = 4 * massMultiplier;
+    var value1 = 2 * massMultiplier;
+    var value2 = 4 * massMultiplier;
 
     // initial positions
     var position1 = -2000;
     var position2 = 2000;
 
-    var modelOptions = {
-      massDensity: 1.5, // kg/m^3
-      massConstantRadius: 500, // m
+    var valueRange = GravityForceLabBasicsConstants.MASS_RANGE;
+    var density = 1.5; // kg/m^3
 
-      valueRange: GravityForceLabBasicsConstants.MASS_RANGE,
+    // @public
+    // TODO: Should this be in InverseSquareLawCommonModel?
+    this.constantRadiusProperty = new Property( false, {
+      tandem: tandem.createTandem( 'constantRadiusProperty' ),
+      phetioValueType: TBoolean
+    } );
 
-      // boundaries for locations of the masses in meters
-      leftObjectBoundary: GravityForceLabBasicsConstants.LEFT_MASS_BOUNDARY,
-      rightObjectBoundary: GravityForceLabBasicsConstants.RIGHT_MASS_BOUNDARY,
+    // TODO: Should these be in inverse-square-law-common?
+    var baseColor1 = new Color( '#00f' );
+    var baseColor2 = new Color( '#f00' );
 
-      snapToNearest: GravityForceLabBasicsConstants.MASS_POSITION_DELTA,
-      minSeparationBetweenObjects: GravityForceLabBasicsConstants.MASS_POSITION_DELTA
-    };
+    var mass1 = new Mass( value1, position1, valueRange, density, this.constantRadiusProperty, baseColor1, tandem );
+    var mass2 = new Mass( value2, position2, valueRange, density, this.constantRadiusProperty, baseColor2, tandem );
 
-    // leverage InverseSquareLawModel, in "Mass" mode
-    InverseSquareLawModel.call( this, InverseSquareLawModes.MASS, mass1, mass2, position1, position2, tandem.createTandem( 'gravityForceLabBasicsModel' ), modelOptions );
+    var leftBoundary = GravityForceLabBasicsConstants.LEFT_MASS_BOUNDARY;
+    var rightBoundary = GravityForceLabBasicsConstants.RIGHT_MASS_BOUNDARY;
 
-    // @public @override
+    InverseSquareLawModel.call( this, InverseSquareLawCommonConstants.G, mass1, mass2, leftBoundary, rightBoundary, tandem.createTandem( 'gravityForceLabBasicsModel' ) );
 
   }
 
