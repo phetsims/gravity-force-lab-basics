48f786097fd0a820e7f6cb839db3aaaa6db16f36
zepumph <michael.kauzmann@colorado.edu>
2019-03-04 10:32:05 -0900

    convert GFLBMassControl to es6, https://github.com/phetsims/gravity-force-lab-basics/issues/96

--- js/gravity-force-lab-basics/view/GFLBMassControl.js
+++ js/gravity-force-lab-basics/view/GFLBMassControl.js
@@ -5,119 +5,118 @@
  *
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var Color = require( 'SCENERY/util/Color' );
-  var GFLBA11yStrings = require( 'GRAVITY_FORCE_LAB_BASICS/gravity-force-lab-basics/GFLBA11yStrings' );
-  var gravityForceLabBasics = require( 'GRAVITY_FORCE_LAB_BASICS/gravityForceLabBasics' );
-  var GFLBConstants = require( 'GRAVITY_FORCE_LAB_BASICS/gravity-force-lab-basics/GFLBConstants' );
-  var GFLBAlertManager = require( 'GRAVITY_FORCE_LAB_BASICS/gravity-force-lab-basics/view/GFLBAlertManager' );
-  var HBox = require( 'SCENERY/nodes/HBox' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var NumberPicker = require( 'SCENERY_PHET/NumberPicker' );
-  var Panel = require( 'SUN/Panel' );
-  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  var Property = require( 'AXON/Property' );
-  var Text = require( 'SCENERY/nodes/Text' );
-  var Util = require( 'DOT/Util' );
-  var VBox = require( 'SCENERY/nodes/VBox' );
+  const Color = require( 'SCENERY/util/Color' );
+  const GFLBA11yStrings = require( 'GRAVITY_FORCE_LAB_BASICS/gravity-force-lab-basics/GFLBA11yStrings' );
+  const gravityForceLabBasics = require( 'GRAVITY_FORCE_LAB_BASICS/gravityForceLabBasics' );
+  const GFLBConstants = require( 'GRAVITY_FORCE_LAB_BASICS/gravity-force-lab-basics/GFLBConstants' );
+  const GFLBAlertManager = require( 'GRAVITY_FORCE_LAB_BASICS/gravity-force-lab-basics/view/GFLBAlertManager' );
+  const HBox = require( 'SCENERY/nodes/HBox' );
+  const NumberPicker = require( 'SCENERY_PHET/NumberPicker' );
+  const Panel = require( 'SUN/Panel' );
+  const PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  const Property = require( 'AXON/Property' );
+  const Text = require( 'SCENERY/nodes/Text' );
+  const Util = require( 'DOT/Util' );
+  const VBox = require( 'SCENERY/nodes/VBox' );
 
   // strings
-  var billionKgString = require( 'string!GRAVITY_FORCE_LAB_BASICS/billionKg' );
+  const billionKgString = require( 'string!GRAVITY_FORCE_LAB_BASICS/billionKg' );
 
   // a11y strings
-  var massReadoutPatternString = GFLBA11yStrings.massReadoutPattern.value;
+  const massReadoutPatternString = GFLBA11yStrings.massReadoutPattern.value;
 
   // constants
-  var MIN_PANEL_WIDTH = 150;
-  var MAX_TEXT_WIDTH = 125; // i18n
+  const MIN_PANEL_WIDTH = 150;
+  const MAX_TEXT_WIDTH = 125; // i18n
 
-  /**
-   * @param {string} titleString
-   * @param {Property.<number>} valueProperty
-   * @param {Range} massRange
-   * @param {String} labelContent - a11y, the content of the label for the mass control
-   * @param {Tandem} tandem
-   * @param {Object} [options]
-   * @constructor
-   */
-  function GFLBMassControl( titleString, valueProperty, massRange, labelContent, tandem, options ) {
+  class GFLBMassControl extends Panel {
 
-    options = _.extend( {
-      color: new Color( 0, 0, 255 )
-    }, options );
+    /**
+     * @param {string} titleString
+     * @param {Property.<number>} valueProperty
+     * @param {Range} massRange
+     * @param {String} labelContent - a11y, the content of the label for the mass control
+     * @param {Tandem} tandem
+     * @param {Object} [options]
+     */
+    constructor( titleString, valueProperty, massRange, labelContent, tandem, options ) {
 
-    const alertManager = GFLBAlertManager.getManager();
+      options = _.extend( {
+        color: new Color( 0, 0, 255 )
+      }, options );
 
-    var titleText = new Text( titleString, {
-      font: new PhetFont( 18 ),
-      maxWidth: MAX_TEXT_WIDTH,
-      tandem: tandem.createTandem( 'titleText' )
-    } );
+      const alertManager = GFLBAlertManager.getManager();
 
-    var numberPicker = new NumberPicker( valueProperty, new Property( massRange ), {
-      font: new PhetFont( 20 ),
-      scale: 1.5,
-      tandem: tandem.createTandem( 'numberPicker' ),
-      upFunction: function( mass ) { return mass + GFLBConstants.BILLION_MULTIPLIER; },
-      downFunction: function( mass ) { return mass - GFLBConstants.BILLION_MULTIPLIER; },
-      formatValue: function( value ) {
-        return Util.toFixed( value / GFLBConstants.BILLION_MULTIPLIER, 0 );
-      },
+      const titleText = new Text( titleString, {
+        font: new PhetFont( 18 ),
+        maxWidth: MAX_TEXT_WIDTH,
+        tandem: tandem.createTandem( 'titleText' )
+      } );
 
-      // arrow options
-      arrowHeight: 3,
-      arrowYSpacing: 2,
-      color: options.color,
+      const numberPicker = new NumberPicker( valueProperty, new Property( massRange ), {
+        font: new PhetFont( 20 ),
+        scale: 1.5,
+        tandem: tandem.createTandem( 'numberPicker' ),
+        upFunction: function( mass ) { return mass + GFLBConstants.BILLION_MULTIPLIER; },
+        downFunction: function( mass ) { return mass - GFLBConstants.BILLION_MULTIPLIER; },
+        formatValue: function( value ) {
+          return Util.toFixed( value / GFLBConstants.BILLION_MULTIPLIER, 0 );
+        },
 
-      // a11y
-      a11yPageValueDelta: GFLBConstants.BILLION_MULTIPLIER * 2,
-      a11yValuePattern: massReadoutPatternString,
-      labelContent: labelContent,
-      a11yFormatValue: function( value ) {
-        return Util.toFixed( value / GFLBConstants.BILLION_MULTIPLIER, 0 );
-      }
-    } );
-    var numberPickerLabel = new Text( billionKgString, {
-      font: new PhetFont( { size: 14 } ),
-      maxWidth: MAX_TEXT_WIDTH,
-      tandem: tandem.createTandem( 'numberPickerLabel' )
-    } );
+        // arrow options
+        arrowHeight: 3,
+        arrowYSpacing: 2,
+        color: options.color,
 
-    // alert on focus
-    numberPicker.addInputListener( {
-      focus() {
-        alertManager.alertMassControlFocused();
-      }
-    } );
+        // a11y
+        a11yPageValueDelta: GFLBConstants.BILLION_MULTIPLIER * 2,
+        a11yValuePattern: massReadoutPatternString,
+        labelContent: labelContent,
+        a11yFormatValue: function( value ) {
+          return Util.toFixed( value / GFLBConstants.BILLION_MULTIPLIER, 0 );
+        }
+      } );
+      const numberPickerLabel = new Text( billionKgString, {
+        font: new PhetFont( { size: 14 } ),
+        maxWidth: MAX_TEXT_WIDTH,
+        tandem: tandem.createTandem( 'numberPickerLabel' )
+      } );
 
-    var numberPickerHBox = new HBox( {
-      children: [ numberPicker, numberPickerLabel ],
-      spacing: 10
-    } );
+      // alert on focus
+      numberPicker.addInputListener( {
+        focus() {
+          alertManager.alertMassControlFocused();
+        }
+      } );
 
-    var panelVBox = new VBox( {
-      children: [ titleText, numberPickerHBox ],
-      spacing: 10
-    } );
+      const numberPickerHBox = new HBox( {
+        children: [ numberPicker, numberPickerLabel ],
+        spacing: 10
+      } );
 
-    Panel.call( this, panelVBox, {
-      fill: '#f1f1f2',
-      xMargin: 15,
-      yMargin: 10,
-      minWidth: MIN_PANEL_WIDTH,
-      resize: false,
-      align: 'center',
-      tandem: tandem,
+      const panelVBox = new VBox( {
+        children: [ titleText, numberPickerHBox ],
+        spacing: 10
+      } );
 
-      // a11y
-      tagName: 'li'
-    } );
+      super( panelVBox, {
+        fill: '#f1f1f2',
+        xMargin: 15,
+        yMargin: 10,
+        minWidth: MIN_PANEL_WIDTH,
+        resize: false,
+        align: 'center',
+        tandem: tandem,
+
+        // a11y
+        tagName: 'li'
+      } );
+    }
   }
 
-  gravityForceLabBasics.register( 'GFLBMassControl', GFLBMassControl );
-
-  return inherit( Panel, GFLBMassControl );
+  return gravityForceLabBasics.register( 'GFLBMassControl', GFLBMassControl );
 } );
