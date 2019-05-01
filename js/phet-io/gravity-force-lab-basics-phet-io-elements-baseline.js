fe549177b0d1a1178b9ded6be5d2eccabea12380
zepumph <michael.kauzmann@colorado.edu>
2019-05-01 13:56:09 -0800

    rename resized and stepSimulation Emitter suffix to Action suffix, https://github.com/phetsims/axon/issues/243

--- js/phet-io/gravity-force-lab-basics-phet-io-elements-baseline.js
+++ js/phet-io/gravity-force-lab-basics-phet-io-elements-baseline.js
@@ -353,7 +353,7 @@ window.phet.phetio.phetioElementsBaseline =
       "phetioStudioControl": true,
       "phetioTypeName": "PhetioCommandProcessorIO"
     },
-    "gravityForceLabBasics.general.resizedEmitter": {
+    "gravityForceLabBasics.general.resizedAction": {
       "phetioDocumentation": "Emits when the sim is resized",
       "phetioEventType": "model",
       "phetioFeatured": false,
@@ -386,7 +386,7 @@ window.phet.phetio.phetioElementsBaseline =
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO.<BooleanIO>"
     },
-    "gravityForceLabBasics.general.stepSimulationEmitter": {
+    "gravityForceLabBasics.general.stepSimulationAction": {
       "phetioDocumentation": "",
       "phetioEventType": "model",
       "phetioFeatured": false,
