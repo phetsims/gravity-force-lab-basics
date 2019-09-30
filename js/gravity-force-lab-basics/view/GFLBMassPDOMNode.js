d815793bfc529c51951cf86d6060a9ed6f3977c0
zepumph <michael.kauzmann@colorado.edu>
2019-09-30 15:11:36 -0800

    fix the third bullet of sphere descriptions, simplifying MassPDOMNode options, https://github.com/phetsims/gravity-force-lab/issues/169

--- js/gravity-force-lab-basics/view/GFLBMassPDOMNode.js
+++ js/gravity-force-lab-basics/view/GFLBMassPDOMNode.js
@@ -8,15 +8,8 @@ define( require => {
   'use strict';
 
   // modules
-  const GravityForceLabA11yStrings = require( 'GRAVITY_FORCE_LAB/gravity-force-lab/GravityForceLabA11yStrings' );
   const gravityForceLabBasics = require( 'GRAVITY_FORCE_LAB_BASICS/gravityForceLabBasics' );
   const MassPDOMNode = require( 'GRAVITY_FORCE_LAB/gravity-force-lab/view/MassPDOMNode' );
-  const Property = require( 'AXON/Property' );
-  const StringUtils = require( 'PHETCOMMON/util/StringUtils' );
-
-  // a11y strings
-  const sizePatternString = GravityForceLabA11yStrings.sizePattern.value;
-  const sizeAndDistancePatternString = GravityForceLabA11yStrings.sizeAndDistancePattern.value;
 
   class GFLBMassPDOMNode extends MassPDOMNode {
 
@@ -29,44 +22,11 @@ define( require => {
      * @param {Object} [options]
      */
     constructor( model, thisObject, massDescriber, forceDescriber, positionDescriber, options ) {
-      super( model, thisObject, massDescriber, forceDescriber, positionDescriber, _.extend( {
+      super( model, thisObject, massDescriber, forceDescriber, positionDescriber, options );
 
-        // The only difference in basics is how the mass/position bullet is updated for this Mass.
-        wireUpMassAndPositionUpdates() {
-          Property.multilink( [
-            model.forceProperty,
-            model.showDistanceProperty,
-            model.constantRadiusProperty,
-
-            // We need to link to these in addition to the forceProperty because of a listener order of ops issue found
-            // in https://github.com/phetsims/gravity-force-lab-basics/issues/103
-            model.object1.positionProperty,
-            model.object2.positionProperty
-          ], () => {
-
-            // The mass/position description in the PDOM differs between GFLB and REGULAR
-            this.massAndPositionNode.innerContent = this.getMassAndPositionSentence();
-          } );
-        }
-      }, options ) );
-    }
-
-    /**
-     * @returns {string}
-     * @private
-     */
-    getMassAndPositionSentence() {
-      const sizeText = StringUtils.fillIn( sizePatternString, {
-        thisObjectLabel: this.massLabel,
-        massValue: this.getMassValue(),
-        unit: this.massDescriber.unit
-      } );
-
-      return StringUtils.fillIn( sizeAndDistancePatternString, {
-        size: sizeText,
-        distance: this.positionDescriber.getDistanceClause( this.objectEnum ),
-        relativeSize: this.massDescriber.getRelativeSizeOrDensity( this.objectEnum ),
-        otherObjectLabel: this.massDescriber.getOtherObjectLabelFromEnum( this.objectEnum )
+      // Update on GFLB specific dependencies.
+      model.showDistanceProperty.link( () => {
+        this.updateMassAndPositionElement();
       } );
     }
   }
