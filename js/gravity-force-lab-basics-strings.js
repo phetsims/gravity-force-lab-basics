0da0fa20a02c1af7bfafba28fd116af58b4d23cd
samreid <reids@colorado.edu>
2020-02-27 12:07:56 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/gravity-force-lab-basics-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import gravityForceLabBasics from './gravityForceLabBasics.js';
+
+const gravityForceLabBasicsStrings = getStringModule( 'GRAVITY_FORCE_LAB_BASICS' );
+
+gravityForceLabBasics.register( 'gravityForceLabBasicsStrings', gravityForceLabBasicsStrings );
+
+export default gravityForceLabBasicsStrings;
