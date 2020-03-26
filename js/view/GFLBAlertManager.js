b208f3e831a05882e379c41635d9b8cef4da77b9
Jesse <jesse.greenberg@colorado.edu>
2020-03-25 20:17:01 -0400

    lazy link so we don't hear speech on load

--- js/view/GFLBAlertManager.js
+++ js/view/GFLBAlertManager.js
@@ -55,7 +55,7 @@ class GFLBAlertManager extends GravityForceLabAlertManager {
         }
       } );
 
-      model.constantRadiusProperty.link( constantRadius => {
+      model.constantRadiusProperty.lazyLink( constantRadius => {
         if ( webSpeaker.getExploreModeVerbose() ) {
           webSpeaker.speak( this.getConstantRadiusAlert( constantRadius ) );
         }
