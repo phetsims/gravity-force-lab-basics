856e3ef08ca16b31f5480cf92e90feea4b57d4c3
zepumph <michael.kauzmann@colorado.edu>
2020-05-07 12:11:34 -0800

    Convert SimLauncher to es6 class, rename to simLauncher.js, https://github.com/phetsims/joist/issues/630

--- js/gravity-force-lab-basics-main.js
+++ js/gravity-force-lab-basics-main.js
@@ -9,7 +9,7 @@
 import GravityForceLabKeyboardHelpContent from '../../gravity-force-lab/js/view/GravityForceLabKeyboardHelpContent.js';
 import Screen from '../../joist/js/Screen.js';
 import Sim from '../../joist/js/Sim.js';
-import SimLauncher from '../../joist/js/SimLauncher.js';
+import simLauncher from '../../joist/js/simLauncher.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import GFLBConstants from './GFLBConstants.js';
 import gravityForceLabBasicsStrings from './gravityForceLabBasicsStrings.js';
@@ -42,7 +42,7 @@ const simOptions = {
   createOptionsDialogContent: ISLCQueryParameters.selfVoicing ? () => new WebSpeechDialogContent() : null
 };
 
-SimLauncher.launch( () => {
+simLauncher.launch( () => {
 
   const gravityForceLabBasicsScreenTandem = tandem.createTandem( 'gravityForceLabBasicsScreen' );
 
