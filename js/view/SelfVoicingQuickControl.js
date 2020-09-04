85491755e19d695c57cf8fd6c71df601aef4d087
Jesse <jesse.greenberg@colorado.edu>
2020-09-04 17:37:01 -0400

    lint fix

--- js/view/SelfVoicingQuickControl.js
+++ js/view/SelfVoicingQuickControl.js
@@ -23,7 +23,6 @@ import HBox from '../../../scenery/js/nodes/HBox.js';
 import Image from '../../../scenery/js/nodes/Image.js';
 import Node from '../../../scenery/js/nodes/Node.js';
 import Rectangle from '../../../scenery/js/nodes/Rectangle.js';
-import Spacer from '../../../scenery/js/nodes/Spacer.js';
 import Text from '../../../scenery/js/nodes/Text.js';
 import VBox from '../../../scenery/js/nodes/VBox.js';
 import BooleanRectangularStickyToggleButton from '../../../sun/js/buttons/BooleanRectangularStickyToggleButton.js';
