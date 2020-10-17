0caf20fe55596c1fc3d960afae685156049eb8fd
samreid <reids@colorado.edu>
2020-10-17 07:09:11 -0600

    Convert phetioComponentOptions: { textProperty} to textPropertyOptions, see https://github.com/phetsims/scenery/issues/1047

--- js/view/DistanceArrowNode.js
+++ js/view/DistanceArrowNode.js
@@ -66,7 +66,7 @@ class DistanceArrowNode extends Node {
       font: new PhetFont( 12 ),
       bottom: arrowNode.top + ( 3 * HEAD_WIDTH / 4 ),
       tandem: options.tandem.createTandem( 'labelText' ),
-      phetioComponentOptions: { textProperty: { phetioReadOnly: true } },
+      textPropertyOptions: { phetioReadOnly: true },
       phetioDocumentation: 'The distance as text between the two masses'
     } );
     this.addChild( labelText );
