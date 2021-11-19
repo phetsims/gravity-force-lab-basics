e1e8b518562ef62cfaa2f9f7761eabb22f6a7a08
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:27 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/gravityForceLabBasicsStrings.ts
@@ -0,0 +1,71 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import gravityForceLabBasics from './gravityForceLabBasics.js';
+
+type StringsType = {
+  'gravity-force-lab-basics': {
+    'title': string
+  },
+  'mass1Label': string,
+  'mass2Label': string,
+  'distanceUnitsPattern': string,
+  'distance': string,
+  'billionKg': string,
+  'mass1ControlLabel': string,
+  'mass2ControlLabel': string,
+  'a11y': {
+    'screenSummary': {
+      'playAreaOverviewPattern': string,
+      'playAreaControls': string,
+      'secondaryDescriptionPattern': string,
+      'basicsSimStateLabel': string,
+      'thePlayAreaHas': string,
+      'inTheControlArea': string
+    },
+    'distanceCheckboxHelpText': string,
+    'massBillionsPattern': string,
+    'kilometer': string,
+    'kilometers': string,
+    'distanceArrowVisible': string,
+    'distanceArrowRemoved': string,
+    'forceArrowsCapitalized': string,
+    'forceArrows': string,
+    'arrows': string,
+    'massControlsHelpTextBillions': string,
+    'massControlsHelpTextDensityBillions': string,
+    'forceArrowCapitalized': string,
+    'sizeOfForce': string,
+    'voicing': {
+      'toolbar': {
+        'overviewPattern': string,
+        'detailsPattern': string,
+        'thereAre': string,
+        'inAddition': string
+      },
+      'distanceKilometersPattern': string,
+      'changeMassHintResponse': string,
+      'massControlReadingBlockPattern': string,
+      'distanceArrowReadingBlockContent': string,
+      'forceValuesHintResponse': string,
+      'distanceHintResponse': string,
+      'constantSizeHintResponse': string,
+      'forceValuesShownResponse': string,
+      'forceValuesHiddenResponse': string,
+      'distanceShownResponse': string,
+      'distanceHiddenResponse': string,
+      'constantSizeSetResponse': string,
+      'constantSizeNotSetResponse': string
+    }
+  }
+};
+
+const gravityForceLabBasicsStrings = getStringModule( 'GRAVITY_FORCE_LAB_BASICS' ) as StringsType;
+
+gravityForceLabBasics.register( 'gravityForceLabBasicsStrings', gravityForceLabBasicsStrings );
+
+export default gravityForceLabBasicsStrings;
