48224c6380b994834b654a05916e0d593afd0e2f
phet-dev <phetadmin@gmail.com>
2022-01-26 03:30:25 -0700

    update copyright dates from daily grunt work

--- js/view/DistanceArrowNode.js
+++ js/view/DistanceArrowNode.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2021, University of Colorado Boulder
+// Copyright 2017-2022, University of Colorado Boulder
 
 /**
  * Double headed arrow that shows the distance between the two masses in gravity-force-lab-basics.  The arrow
