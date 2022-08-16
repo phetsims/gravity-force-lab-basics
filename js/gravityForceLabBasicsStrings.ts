4b7b54e9f2e2ca2a8d0321821767c868730c6299
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:33 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/gravityForceLabBasicsStrings.ts
+++ js/gravityForceLabBasicsStrings.ts
@@ -5,61 +5,106 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import gravityForceLabBasics from './gravityForceLabBasics.js';
 
 type StringsType = {
   'gravity-force-lab-basics': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'mass1Label': string;
+  'mass1LabelProperty': TReadOnlyProperty<string>;
   'mass2Label': string;
+  'mass2LabelProperty': TReadOnlyProperty<string>;
   'distanceUnitsPattern': string;
+  'distanceUnitsPatternProperty': TReadOnlyProperty<string>;
   'distance': string;
+  'distanceProperty': TReadOnlyProperty<string>;
   'billionKg': string;
+  'billionKgProperty': TReadOnlyProperty<string>;
   'mass1ControlLabel': string;
+  'mass1ControlLabelProperty': TReadOnlyProperty<string>;
   'mass2ControlLabel': string;
+  'mass2ControlLabelProperty': TReadOnlyProperty<string>;
   'a11y': {
     'screenSummary': {
       'playAreaOverviewPattern': string;
+      'playAreaOverviewPatternProperty': TReadOnlyProperty<string>;
       'playAreaControls': string;
+      'playAreaControlsProperty': TReadOnlyProperty<string>;
       'secondaryDescriptionPattern': string;
+      'secondaryDescriptionPatternProperty': TReadOnlyProperty<string>;
       'basicsSimStateLabel': string;
+      'basicsSimStateLabelProperty': TReadOnlyProperty<string>;
       'thePlayAreaHas': string;
+      'thePlayAreaHasProperty': TReadOnlyProperty<string>;
       'inTheControlArea': string;
+      'inTheControlAreaProperty': TReadOnlyProperty<string>;
     };
     'distanceCheckboxHelpText': string;
+    'distanceCheckboxHelpTextProperty': TReadOnlyProperty<string>;
     'massBillionsPattern': string;
+    'massBillionsPatternProperty': TReadOnlyProperty<string>;
     'kilometer': string;
+    'kilometerProperty': TReadOnlyProperty<string>;
     'kilometers': string;
+    'kilometersProperty': TReadOnlyProperty<string>;
     'distanceArrowVisible': string;
+    'distanceArrowVisibleProperty': TReadOnlyProperty<string>;
     'distanceArrowRemoved': string;
+    'distanceArrowRemovedProperty': TReadOnlyProperty<string>;
     'forceArrowsCapitalized': string;
+    'forceArrowsCapitalizedProperty': TReadOnlyProperty<string>;
     'forceArrows': string;
+    'forceArrowsProperty': TReadOnlyProperty<string>;
     'arrows': string;
+    'arrowsProperty': TReadOnlyProperty<string>;
     'massControlsHelpTextBillions': string;
+    'massControlsHelpTextBillionsProperty': TReadOnlyProperty<string>;
     'massControlsHelpTextDensityBillions': string;
+    'massControlsHelpTextDensityBillionsProperty': TReadOnlyProperty<string>;
     'forceArrowCapitalized': string;
+    'forceArrowCapitalizedProperty': TReadOnlyProperty<string>;
     'sizeOfForce': string;
+    'sizeOfForceProperty': TReadOnlyProperty<string>;
     'voicing': {
       'toolbar': {
         'overviewPattern': string;
+        'overviewPatternProperty': TReadOnlyProperty<string>;
         'detailsPattern': string;
+        'detailsPatternProperty': TReadOnlyProperty<string>;
         'thereAre': string;
+        'thereAreProperty': TReadOnlyProperty<string>;
         'inAddition': string;
+        'inAdditionProperty': TReadOnlyProperty<string>;
       };
       'distanceKilometersPattern': string;
+      'distanceKilometersPatternProperty': TReadOnlyProperty<string>;
       'changeMassHintResponse': string;
+      'changeMassHintResponseProperty': TReadOnlyProperty<string>;
       'massControlReadingBlockPattern': string;
+      'massControlReadingBlockPatternProperty': TReadOnlyProperty<string>;
       'distanceArrowReadingBlockNameResponse': string;
+      'distanceArrowReadingBlockNameResponseProperty': TReadOnlyProperty<string>;
       'forceValuesHintResponse': string;
+      'forceValuesHintResponseProperty': TReadOnlyProperty<string>;
       'distanceHintResponse': string;
+      'distanceHintResponseProperty': TReadOnlyProperty<string>;
       'constantSizeHintResponse': string;
+      'constantSizeHintResponseProperty': TReadOnlyProperty<string>;
       'forceValuesShownResponse': string;
+      'forceValuesShownResponseProperty': TReadOnlyProperty<string>;
       'forceValuesHiddenResponse': string;
+      'forceValuesHiddenResponseProperty': TReadOnlyProperty<string>;
       'distanceShownResponse': string;
+      'distanceShownResponseProperty': TReadOnlyProperty<string>;
       'distanceHiddenResponse': string;
+      'distanceHiddenResponseProperty': TReadOnlyProperty<string>;
       'constantSizeSetResponse': string;
+      'constantSizeSetResponseProperty': TReadOnlyProperty<string>;
       'constantSizeNotSetResponse': string;
+      'constantSizeNotSetResponseProperty': TReadOnlyProperty<string>;
     }
   }
 };
